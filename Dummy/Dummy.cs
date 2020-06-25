2f545b350abcb18a6afc00f3eef4bb76849a4e00
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-06-25 19:25:41 +0700

    Add project files.

---
+++ Dummy/Dummy.cs
@@ -0,0 +1,29 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy
+{
+    public class Dummy : RocketPlugin
+    {
+        public static Dummy Instance;
+
+        protected override void Load()
+        {
+            Instance = this;
+
+            Logger.Log("Made with <3 by Evolution Plugins", ConsoleColor.Cyan);
+            Logger.Log("https://vk.com/evolutionplugins", ConsoleColor.Cyan);
+            Logger.Log("Discord: ", ConsoleColor.Cyan);
+        }
+
+        protected override void Unload()
+        {
+            
+        }
+    }
+}
---
+++ Dummy/Patches/Patch_Provider_send.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy.Patches
+{
+    [HarmonyPatch(typeof(Provider), "send")]
+    public static class Patch_Provider_send
+    {
+        public static bool Prefix(CSteamID steamID)
+        {
+            return steamID == CSteamID.Nil;
+        }
+    }
+}
