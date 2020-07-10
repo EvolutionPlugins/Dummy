414560e5552172a5b0ff942e730470fa76bec088
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-07-11 02:39:43 +0700

    Implement command gesture

--- Dummy/Commands/CommandDummyGesture.cs
+++ Dummy/Commands/CommandDummyGesture.cs
@@ -1,25 +1,49 @@
-﻿using OpenMod.Core.Commands;
+﻿using EvolutionPlugins.Dummy.API;
+using OpenMod.API.Commands;
+using OpenMod.Core.Commands;
+using OpenMod.Unturned.Users;
+using SDG.Unturned;
+using Steamworks;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Command = OpenMod.Core.Commands.Command;
 
 namespace EvolutionPlugins.Dummy.Commands
 {
     [Command("gesture")]
     [CommandDescription("Make a dummy gesture")]
     [CommandParent(typeof(CommandDummy))]
-    [CommandSyntax("<gesture>")]
+    [CommandSyntax("<id> <gesture>")]
     public class CommandDummyGesture : Command
     {
-        public CommandDummyGesture(IServiceProvider serviceProvider) : base(serviceProvider)
+        private readonly IDummyProvider m_DummyProvider;
+
+        public CommandDummyGesture(IServiceProvider serviceProvider, IDummyProvider dummyProvider) : base(serviceProvider)
         {
+            m_DummyProvider = dummyProvider;
         }
 
-        protected override Task OnExecuteAsync()
+        protected override async Task OnExecuteAsync()
         {
-            throw new NotImplementedException();
+            if (Context.Parameters.Count == 0)
+            {
+                throw new CommandWrongUsageException(Context);
+            }
+            var id = (CSteamID)await Context.Parameters.GetAsync<ulong>(0);
+
+            var dummy = (UnturnedUser)await m_DummyProvider.FindDummyAsync(id.m_SteamID);
+            if (dummy == null)
+            {
+                throw new UserFriendlyException($"Dummy \"{id}\" has not found!");
+            }
+
+            var gesture = await Context.Parameters.GetAsync<string>(1);
+            if (!Enum.TryParse<EPlayerGesture>(gesture.ToUpper(), out var eGesture))
+            {
+                throw new UserFriendlyException($"Unable find a gesture {gesture}");
+            }
+
+            dummy.Player.animator.sendGesture(eGesture, false);
         }
     }
 }
