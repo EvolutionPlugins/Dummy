86a4729a7c9a52ba808888f96351115e2aaa9b84
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-07-10 09:21:58 +0700

    Implement command face

--- Dummy/Commands/CommandDummyFace.cs
+++ Dummy/Commands/CommandDummyFace.cs
@@ -1,25 +1,59 @@
-﻿using OpenMod.Core.Commands;
+﻿using EvolutionPlugins.Dummy.API;
+using OpenMod.API.Commands;
+using OpenMod.API.Users;
+using OpenMod.Core.Commands;
+using OpenMod.Core.Users;
+using OpenMod.Unturned.Users;
+using SDG.Unturned;
+using Steamworks;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Command = OpenMod.Core.Commands.Command;
 
 namespace EvolutionPlugins.Dummy.Commands
 {
     [Command("face")]
     [CommandDescription("Send a face to dummy")]
     [CommandParent(typeof(CommandDummy))]
-    [CommandSyntax("<face>")]
+    [CommandSyntax("<id> <face>")]
     public class CommandDummyFace : Command
     {
-        public CommandDummyFace(IServiceProvider serviceProvider) : base(serviceProvider)
+        private readonly IDummyProvider m_DummyProvider;
+        private readonly IUserManager m_UserManager;
+
+        public CommandDummyFace(IServiceProvider serviceProvider, IDummyProvider dummyProvider, IUserManager userManager) : base(serviceProvider)
         {
+            m_DummyProvider = dummyProvider;
+            m_UserManager = userManager;
         }
 
-        protected override Task OnExecuteAsync()
+        protected override async Task OnExecuteAsync()
         {
-            throw new NotImplementedException();
+            if (Context.Parameters.Count == 0)
+            {
+                throw new CommandWrongUsageException(Context);
+            }
+
+            var id = await Context.Parameters.GetAsync<CSteamID>(0);
+
+            if (!m_DummyProvider.Dummies.TryGetValue(id, out _))
+            {
+                throw new UserFriendlyException($"Dummy \"{id}\" has not found!");
+            }
+
+            var dummy = (UnturnedUser)await m_UserManager.FindUserAsync(KnownActorTypes.Player, id.ToString(), UserSearchMode.Id);
+
+            var faceId = await Context.Parameters.GetAsync<byte>(1);
+
+            if (faceId > Customization.FACES_FREE + Customization.FACES_PRO)
+            {
+                throw new UserFriendlyException($"Can't change to {faceId} because is higher {Customization.FACES_FREE + Customization.FACES_PRO}");
+            }
+
+            dummy.Player.clothing.channel.send("tellSwapFace", ESteamCall.NOT_OWNER, ESteamPacket.UPDATE_RELIABLE_BUFFER, new object[]
+            {
+                faceId
+            });
         }
     }
 }
