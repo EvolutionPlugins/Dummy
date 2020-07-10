e5710fff9046bb35fd43ec7be277c46f8023b715
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-07-10 09:05:16 +0700

    Implement copy command

--- Dummy/Commands/CommandDummyCopy.cs
+++ Dummy/Commands/CommandDummyCopy.cs
@@ -1,15 +1,12 @@
-﻿using EvolutionPlugins.Dummy.Providers;
+﻿using Cysharp.Threading.Tasks;
+using EvolutionPlugins.Dummy.API;
 using Microsoft.Extensions.Configuration;
-using OpenMod.API.Commands;
-using OpenMod.API.Users;
 using OpenMod.Core.Commands;
-using OpenMod.Core.Users;
 using OpenMod.Unturned.Users;
 using SDG.Unturned;
 using Steamworks;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using Color = System.Drawing.Color;
@@ -24,167 +21,50 @@ namespace EvolutionPlugins.Dummy.Commands
     [CommandParent(typeof(CommandDummy))]
     public class CommandDummyCopy : Command
     {
-        public CommandDummyCopy(IServiceProvider serviceProvider) : base(serviceProvider)
-        {
+        private readonly IConfiguration m_Configuration;
+        private readonly IDummyProvider m_DummyProvider;
 
+        public CommandDummyCopy(IServiceProvider serviceProvider, IConfiguration configuration,
+            IDummyProvider dummyProvider) : base(serviceProvider)
+        {
+            m_Configuration = configuration;
+            m_DummyProvider = dummyProvider;
         }
 
-        protected override Task OnExecuteAsync()
+        protected override async Task OnExecuteAsync()
         {
-            throw new NotImplementedException();
+            var user = (UnturnedUser)Context.Actor;
+            var amountDummiesConfig = m_Configuration.GetSection("AmountDummiesInSameTime").Get<byte>();
+            if (amountDummiesConfig != 0 && m_DummyProvider.Dummies.Count + 1 > amountDummiesConfig)
+            {
+                await user.PrintMessageAsync("Dummy can't be created. Amount dummies overflow", Color.Yellow);
+                return;
+            }
+
+            var id = m_DummyProvider.GetAvailableId();
+
+            await m_DummyProvider.AddDummyAsync(id, new DummyData() { Owners = new List<CSteamID> { user.SteamId } });
+
+            var steamPlayer = user.SteamPlayer;
+
+            Provider.pending.Add(new SteamPending(new SteamPlayerID(id, 0, "dummy", "dummy", "dummy", CSteamID.Nil),
+                true, steamPlayer.face, steamPlayer.hair, steamPlayer.beard, steamPlayer.skin, steamPlayer.color,
+                UnityEngine.Color.white, steamPlayer.hand, 0UL, 0UL, 0UL, 0UL, 0UL, 0UL, 0UL, Array.Empty<ulong>(),
+                EPlayerSkillset.NONE, "english", CSteamID.Nil));
+
+            await UniTask.SwitchToMainThread();
+            Provider.accept(new SteamPlayerID(id, 0, "dummy", "dummy", "dummy", CSteamID.Nil), true, false,
+                steamPlayer.face, steamPlayer.hair, steamPlayer.beard, steamPlayer.skin, steamPlayer.color,
+                UnityEngine.Color.white, steamPlayer.hand, steamPlayer.shirtItem, steamPlayer.pantsItem, steamPlayer.hatItem,
+                steamPlayer.backpackItem, steamPlayer.vestItem, steamPlayer.maskItem, steamPlayer.glassesItem,
+                steamPlayer.skinItems, steamPlayer.skinTags, steamPlayer.skinDynamicProps, EPlayerSkillset.NONE,
+                "english", CSteamID.Nil);
+
+            var dummy = Provider.clients.Last();
+            dummy.player.teleportToLocationUnsafe(user.Player.transform.position, user.Player.transform.rotation.eulerAngles.y);
+            await UniTask.SwitchToTaskPool();
+
+            await user.PrintMessageAsync($"Dummy ({id.m_SteamID}) has created");
         }
     }
-    // TODO:
-    /*private void FaceDummy(UnturnedPlayer player, byte id, byte faceId)
-        {
-            if (!global::Dummy.Instance.Dummies.ContainsKey((CSteamID)id))
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-            var dummy = Provider.clients.Find(k => k.playerID.steamID.m_SteamID == id);
-            if (dummy == null)
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-            
-            if (faceId > Customization.FACES_FREE + Customization.FACES_PRO)
-            {
-                UnturnedChat.Say(player, $"Can't change to {faceId} because is higher {Customization.FACES_FREE + Customization.FACES_PRO}", Color.red);
-                return;
-            }
-
-            dummy.player.clothing.channel.send("tellSwapFace", ESteamCall.NOT_OWNER, ESteamPacket.UPDATE_RELIABLE_BUFFER, new object[]
-            {
-                faceId
-            });
-        }
-
-        private void StanceDummy(UnturnedPlayer player, byte id, string stance)
-        {
-            if (!global::Dummy.Instance.Dummies.ContainsKey((CSteamID)id))
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-            var dummy = Provider.clients.Find(k => k.playerID.steamID.m_SteamID == id);
-            if (dummy == null)
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-
-            if (!Enum.TryParse<EPlayerStance>(stance.ToUpper(), out var eStance))
-            {
-                UnturnedChat.Say(player, $"Unable find a stance {stance}", Color.red);
-                return;
-            }
-
-            dummy.player.stance.checkStance(eStance, false);
-        }
-
-        private void GestureDummy(UnturnedPlayer player, byte id, string gesture)
-        {
-            if (!global::Dummy.Instance.Dummies.ContainsKey((CSteamID)id))
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-            var dummy = Provider.clients.Find(k => k.playerID.steamID.m_SteamID == id);
-            if (dummy == null)
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-
-            if (!Enum.TryParse<EPlayerGesture>(gesture.ToUpper(), out var eGesture))
-            {
-                UnturnedChat.Say(player, $"Unable find a gesture {gesture}", Color.red);
-                return;
-            }
-
-            dummy.player.animator.sendGesture(eGesture, false);
-        }
-
-        private void TeleportDummy(UnturnedPlayer player, byte id)
-        {
-            if (!global::Dummy.Instance.Dummies.ContainsKey((CSteamID)id))
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-            var dummy = Provider.clients.Find(k => k.playerID.steamID.m_SteamID == id);
-            if (dummy == null)
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-
-            dummy.player.transform.position = player.Position;
-            dummy.player.transform.rotation = player.Player.transform.rotation;
-
-            (dummy.player.movement.updates
-             ?? (dummy.player.movement.updates = new List<PlayerStateUpdate>())).Add(new PlayerStateUpdate(player.Position,
-                player.Player.look.angle, player.Player.look.rot));
-        }
-
-        private void ExecuteCommandDummy(UnturnedPlayer player, byte id, string command)
-        {
-            if (!global::Dummy.Instance.Dummies.ContainsKey((CSteamID)id))
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-
-            var x = R.Commands.Execute(UnturnedPlayer.FromCSteamID((CSteamID)id), command);
-
-            ChatManager.serverSendMessage($"Dummy ({id}) has {(x ? "successfully" : "<color=red>unsuccessfully</color>")} executed command", Color.green, toPlayer: player.SteamPlayer(), useRichTextFormatting: true);
-        }
-
-        private void ClearAllDummies(UnturnedPlayer player)
-        {
-            foreach (var dummy in global::Dummy.Instance.Dummies)
-            {
-                var _dummy = Provider.clients.Find(k => k.playerID.steamID == dummy.Key);
-                if (_dummy == null)
-                {
-                    UnturnedChat.Say(player, $"Dummy ({_dummy.playerID.steamID}) failed to remove!", Color.red);
-                    continue;
-                }
-                if (dummy.Value.Coroutine != null)
-                    global::Dummy.Instance.StopCoroutine(dummy.Value.Coroutine);
-
-                Provider.kick(_dummy.playerID.steamID, "");
-            }
-
-            global::Dummy.Instance.Dummies.Clear();
-            UnturnedChat.Say(player, "Dummies were removed", Color.green);
-        }
-
-        private void RemoveDummy(UnturnedPlayer player, byte id)
-        {
-            if (!global::Dummy.Instance.Dummies.Any((object k) => k.Key.m_SteamID == id))
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-            }
-            var data = global::Dummy.Instance.Dummies[(CSteamID)id];
-
-            var dummy = Provider.clients.Find(k => k.playerID.steamID.m_SteamID == id);
-            if (dummy == null)
-            {
-                UnturnedChat.Say(player, $"Dummy ({id}) not found", Color.red);
-                return;
-            }
-
-            Provider.kick(dummy.playerID.steamID, "");
-
-            if (data.Coroutine != null)
-                global::Dummy.Instance.StopCoroutine(data.Coroutine);
-
-            global::Dummy.Instance.Dummies.Remove(new CSteamID(id));
-            UnturnedChat.Say(player, $"Dummy ({id}) was removed", Color.green);
-        }*/
-
-
 }
