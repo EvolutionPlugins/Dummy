24fcc42ca4e509e1489e0135ba36b41e36a494b1
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-07-10 22:37:54 +0700

    Move events to provider

--- Dummy/Dummy.cs
+++ Dummy/Dummy.cs
@@ -2,17 +2,12 @@
 using EvolutionPlugins.Dummy.API;
 using EvolutionPlugins.Dummy.Patches;
 using HarmonyLib;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using OpenMod.API.Plugins;
-using OpenMod.API.Users;
 using OpenMod.Core.Helpers;
-using OpenMod.Core.Users;
 using OpenMod.Unturned.Plugins;
 using SDG.Unturned;
-using Steamworks;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
@@ -27,16 +22,12 @@ namespace EvolutionPlugins.Dummy
 
         private readonly Harmony m_Harmony;
         private readonly ILogger m_Logger;
-        private readonly IConfiguration m_Configuration;
-        private readonly IUserProvider m_UserProvider;
         private readonly IDummyProvider m_DummyProvider;
 
-        public Dummy(IServiceProvider serviceProvider, ILogger<Dummy> logger, IUserManager userManager, IDummyProvider dummyProvider) : base(serviceProvider)
+        public Dummy(IServiceProvider serviceProvider, ILogger<Dummy> logger, IDummyProvider dummyProvider) : base(serviceProvider)
         {
             m_Harmony = new Harmony(_HarmonyId);
             m_Logger = logger;
-            m_Configuration = Configuration;
-            m_UserProvider = userManager.UserProviders.FirstOrDefault(x => x.SupportsUserType(KnownActorTypes.Player));
             m_DummyProvider = dummyProvider;
         }
 
@@ -54,16 +45,7 @@ namespace EvolutionPlugins.Dummy
 
             m_Harmony.PatchAll();
 
-            DamageTool.damagePlayerRequested += DamageTool_damagePlayerRequested;
-            Provider.onServerDisconnected += OnServerDisconnected;
-            ChatManager.onServerSendingMessage += OnServerSendingMessage;
-
             return UniTask.CompletedTask;
-        }
-
-        private IDummyProvider GiveProvider()
-        {
-            return m_DummyProvider;
         }
 
         protected override UniTask OnUnloadAsync()
@@ -74,65 +56,7 @@ namespace EvolutionPlugins.Dummy
             Patch_Provider_send.OnNeedProvider -= GiveProvider;
             Patch_Provider_verifyNextPlayerInQueue.OnNeedProvider -= GiveProvider;
 
-            DamageTool.damagePlayerRequested -= DamageTool_damagePlayerRequested;
-            Provider.onServerDisconnected -= OnServerDisconnected;
-            ChatManager.onServerSendingMessage -= OnServerSendingMessage;
-
             return UniTask.CompletedTask;
-        }
-
-        private void OnServerSendingMessage(ref string text, ref Color color, SteamPlayer fromPlayer, SteamPlayer toPlayer, EChatMode mode, ref string iconURL, ref bool useRichTextFormatting)
-        {
-            if (toPlayer == null)
-            {
-                return;
-            }
-
-            if (!m_DummyProvider.Dummies.ContainsKey(toPlayer.playerID.steamID))
-            {
-                return;
-            }
-
-            var data = m_DummyProvider.Dummies[toPlayer.playerID.steamID];
-
-            foreach (var owner in data.Owners)
-            {
-                var steamPlayerOwner = PlayerTool.getSteamPlayer(owner);
-                if(steamPlayerOwner == null)
-                {
-                    continue;
-                }
-                ChatManager.serverSendMessage($"Dummy {toPlayer.playerID.steamID} got message: {text}", color, toPlayer: steamPlayerOwner, iconURL: iconURL, useRichTextFormatting: true);
-            }
-        }
-
-        private void OnServerDisconnected(CSteamID steamID)
-        {
-            AsyncHelper.RunSync(() => m_DummyProvider.RemoveDummyAsync(steamID));
-        }
-
-        private void DamageTool_damagePlayerRequested(ref DamagePlayerParameters parameters, ref bool shouldAllow)
-        {
-            if (!m_DummyProvider.Dummies.ContainsKey(parameters.player.channel.owner.playerID.steamID))
-            {
-                return;
-            }
-            shouldAllow = false;
-            var totalTimes = parameters.times;
-
-            if (parameters.respectArmor)
-            {
-                totalTimes *= DamageTool.getPlayerArmor(parameters.limb, parameters.player);
-            }
-            if (parameters.applyGlobalArmorMultiplier)
-            {
-                totalTimes *= Provider.modeConfigData.Players.Armor_Multiplier;
-            }
-            var totalDamage = (byte)Mathf.Min(255, parameters.damage * totalTimes);
-
-            var killerId = parameters.killer;
-
-            ChatManager.say(killerId, $"Amount damage to dummy: {totalDamage}", Color.green, true);
         }
 
         private async Task DontAutoKickTask()
@@ -150,6 +74,11 @@ namespace EvolutionPlugins.Dummy
                 }
                 await Task.Delay(5000);
             }
+        }
+
+        private IDummyProvider GiveProvider()
+        {
+            return m_DummyProvider;
         }
     }
 }
--- Dummy/Providers/DummyProvider.cs
+++ Dummy/Providers/DummyProvider.cs
@@ -3,19 +3,18 @@ using EvolutionPlugins.Dummy.Models;
 using Microsoft.Extensions.DependencyInjection;
 using OpenMod.API.Ioc;
 using OpenMod.API.Prioritization;
+using OpenMod.Core.Helpers;
 using SDG.Unturned;
 using Steamworks;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace EvolutionPlugins.Dummy.Providers
 {
     [ServiceImplementation(Lifetime = ServiceLifetime.Singleton, Priority = Priority.Lowest)]
-#pragma warning disable CA1063 // Implement IDisposable Correctly
-    public class DummyProvider : IDummyProvider, IDisposable
-#pragma warning restore CA1063 // Implement IDisposable Correctly
+    public class DummyProvider : IDummyProvider, IAsyncDisposable
     {
         private bool m_IsDisposing;
         private readonly Dictionary<CSteamID, DummyData> m_Dummies;
@@ -26,6 +25,66 @@ namespace EvolutionPlugins.Dummy.Providers
         {
             m_Dummies = new Dictionary<CSteamID, DummyData>();
             m_IsDisposing = false;
+
+            Provider.onServerDisconnected += OnServerDisconnected;
+            ChatManager.onServerSendingMessage += OnServerSendingMessage;
+            DamageTool.damagePlayerRequested += DamageTool_damagePlayerRequested;
+        }
+
+        protected virtual void OnServerSendingMessage(ref string text, ref Color color, SteamPlayer fromPlayer,
+            SteamPlayer toPlayer, EChatMode mode, ref string iconURL, ref bool useRichTextFormatting)
+        {
+            if (toPlayer == null)
+            {
+                return;
+            }
+
+            if (!Dummies.ContainsKey(toPlayer.playerID.steamID))
+            {
+                return;
+            }
+
+            var data = Dummies[toPlayer.playerID.steamID];
+
+            foreach (var owner in data.Owners)
+            {
+                var steamPlayerOwner = PlayerTool.getSteamPlayer(owner);
+                if (steamPlayerOwner == null)
+                {
+                    continue;
+                }
+                ChatManager.serverSendMessage($"Dummy {toPlayer.playerID.steamID} got message: {text}", color,
+                    toPlayer: steamPlayerOwner, iconURL: iconURL, useRichTextFormatting: true);
+            }
+        }
+
+        protected virtual void OnServerDisconnected(CSteamID steamID)
+        {
+            AsyncHelper.RunSync(() => RemoveDummyAsync(steamID));
+        }
+
+        protected virtual void DamageTool_damagePlayerRequested(ref DamagePlayerParameters parameters, ref bool shouldAllow)
+        {
+            if (!Dummies.ContainsKey(parameters.player.channel.owner.playerID.steamID))
+            {
+                return;
+            }
+            shouldAllow = false;
+            var totalTimes = parameters.times;
+
+            if (parameters.respectArmor)
+            {
+                totalTimes *= DamageTool.getPlayerArmor(parameters.limb, parameters.player);
+            }
+            if (parameters.applyGlobalArmorMultiplier)
+            {
+                totalTimes *= Provider.modeConfigData.Players.Armor_Multiplier;
+            }
+            var totalDamage = (byte)Mathf.Min(255, parameters.damage * totalTimes);
+
+            var killerId = parameters.killer;
+
+            ChatManager.say(killerId, $"Amount damage to dummy: {totalDamage}", Color.green, true);
         }
 
         public Task<bool> AddDummyAsync(CSteamID Id, DummyData dummyData)
@@ -35,18 +94,7 @@ namespace EvolutionPlugins.Dummy.Providers
                 return Task.FromResult(false);
             }
             m_Dummies.Add(Id, dummyData);
-            return Task.FromResult(false);
-        }
-
-        public CSteamID GetAvailableId()
-        {
-            var result = new CSteamID(1);
-
-            while (Dummies.ContainsKey(result))
-            {
-                result.m_SteamID++;
-            }
-            return result;
+            return Task.FromResult(true);
         }
 
         public Task<bool> RemoveDummyAsync(CSteamID Id)
@@ -60,23 +108,35 @@ namespace EvolutionPlugins.Dummy.Providers
             return Task.CompletedTask;
         }
 
-#pragma warning disable CA1063 // Implement IDisposable Correctly
-        public void Dispose()
-#pragma warning restore CA1063 // Implement IDisposable Correctly
+        public CSteamID GetAvailableId()
+        {
+            var result = new CSteamID(1);
+
+            while (Dummies.ContainsKey(result))
+            {
+                result.m_SteamID++;
+            }
+            return result;
+        }
+
+        public ValueTask DisposeAsync()
         {
             if (m_IsDisposing)
             {
-                return;
+                return new ValueTask(Task.CompletedTask);
             }
             m_IsDisposing = true;
 
-            var keys = m_Dummies.Keys.ToList();
-            foreach (var cSteamID in keys)
+            Provider.onServerDisconnected -= OnServerDisconnected;
+            ChatManager.onServerSendingMessage -= OnServerSendingMessage;
+            DamageTool.damagePlayerRequested -= DamageTool_damagePlayerRequested;
+
+            foreach (var cSteamID in m_Dummies.Keys)
             {
                 Provider.kick(cSteamID, "");
             }
 
-            m_Dummies.Clear();
+            return new ValueTask(ClearAllDummies());
         }
     }
 }
