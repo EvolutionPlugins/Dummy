e7cae2d91b968a4dfa5ddaddca8b35974bb688ec
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-08-24 06:57:01 +0700

    Add sprint action

---
+++ Dummy/Extensions/Movement/Actions/SprintAction.cs
@@ -0,0 +1,17 @@
+﻿using EvolutionPlugins.Dummy;
+using EvolutionPlugins.Dummy.Extensions.Movement;
+using SDG.Unturned;
+using UnityEngine;
+
+namespace Dummy.Extensions.Movement.Actions
+{
+    public class SprintAction : IMovementAction
+    {
+        public void Do(PlayerDummy dummy)
+        {
+            var player = dummy.Data.UnturnedUser.Player;
+            player.movement.simulate(1, 0, player.movement.horizontal - 1, player.movement.vertical - 1,
+                player.look.look_x, player.look.look_y, false, true, Vector3.zero, PlayerInput.RATE);
+        }
+    }
+}
