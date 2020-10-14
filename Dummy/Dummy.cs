3ace4cf4a4cec5464c6791ba96b9794dcce8fdbd
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-10-14 14:32:04 +0700

    Log discord server

--- Dummy/Dummy.cs
+++ Dummy/Dummy.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Logging;
 using OpenMod.API.Plugins;
 using OpenMod.Unturned.Plugins;
 using System;
-using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 [assembly: PluginMetadata("Dummy", Author = "DiFFoZ", DisplayName = "Dummy",
     Website = "https://github.com/EvolutionPlugins/Dummy")]
@@ -16,7 +15,7 @@ namespace Dummy
 
     public class Dummy : OpenModUnturnedPlugin
     {
-        private readonly ILogger m_Logger;
+        private readonly ILogger<Dummy> m_Logger;
         private readonly IDummyProvider m_DummyProvider;
 
         public Dummy(IServiceProvider serviceProvider, ILogger<Dummy> logger, IDummyProvider dummyProvider) : base(serviceProvider)
@@ -31,7 +30,7 @@ namespace Dummy
 
             m_Logger.LogInformation("Made with <3 by Evolution Plugins");
             m_Logger.LogInformation("https://github.com/evolutionplugins \\ https://github.com/diffoz");
-            m_Logger.LogInformation("Discord: DiFFoZ#6745");
+            m_Logger.LogInformation("Discord: DiFFoZ#6745 \\ https://discord.gg/6KymqGv");
             return UniTask.CompletedTask;
         }
 
