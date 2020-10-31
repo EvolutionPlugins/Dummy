4961786107882607d7099647c135fcd8e2446171
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-11-01 06:36:03 +0700

    Add some actions. Add latest libs of Unturned.

---
+++ Dummy/Extensions/Interaction/Actions/Vehicle/ExitVehicleAction.cs
@@ -0,0 +1,33 @@
+﻿using Cysharp.Threading.Tasks;
+using Dummy.Users;
+using SDG.Unturned;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Dummy.Extensions.Interaction.Actions.Vehicle
+{
+    public class ExitVehicleAction : IInteractionAction
+    {
+        private readonly InteractableVehicle m_InteractableVehicle;
+        public uint InstanceId { get; }
+
+        public ExitVehicleAction(uint instanceId)
+        {
+            InstanceId = instanceId;
+            m_InteractableVehicle = VehicleManager.findVehicleByNetInstanceID(instanceId)
+                ?? throw new ArgumentException(nameof(m_InteractableVehicle));
+        }
+
+        public ExitVehicleAction(InteractableVehicle interactableVehicle)
+        {
+            InstanceId = interactableVehicle.instanceID;
+        }
+
+        public async Task Do(DummyUser dummy)
+        {
+            await UniTask.SwitchToMainThread();
+            VehicleManager.instance.askExitVehicle(dummy.SteamID, Vector3.zero);
+        }
+    }
+}
