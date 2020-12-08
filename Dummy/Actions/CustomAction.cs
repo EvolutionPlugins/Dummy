0194327674bc5b2dd8ba194fdaef7a2300229d68
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-12-08 15:04:05 +0700

    Add custom action

---
+++ Dummy/Actions/CustomAction.cs
@@ -0,0 +1,22 @@
+﻿using Dummy.API;
+using Dummy.Users;
+using System;
+using System.Threading.Tasks;
+
+namespace Dummy.Actions
+{
+    public class CustomAction : IAction
+    {
+        public CustomAction(Func<DummyUser, Task> func)
+        {
+            Func = func;
+        }
+
+        public Func<DummyUser, Task> Func { get; }
+
+        public Task Do(DummyUser dummy)
+        {
+            return Func(dummy);
+        }
+    }
+}
