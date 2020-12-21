e2ada9439ff2a15b4e7c5dd205615088ba4714e1
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-12-21 20:46:02 +0700

    Add look command, closes #16

---
+++ Dummy/Actions/Movement/Actions/RotateAction.cs
@@ -0,0 +1,25 @@
+﻿using Dummy.API;
+using Dummy.Users;
+using System.Threading.Tasks;
+
+namespace Dummy.Actions.Movement.Actions
+{
+    public class RotateAction : IAction
+    {
+        public RotateAction(float yaw, float pitch)
+        {
+            Yaw = yaw;
+            Pitch = pitch;
+        }
+
+        public float Yaw { get; }
+        public float Pitch { get; }
+
+        public Task Do(DummyUser dummy)
+        {
+            dummy.Simulation.Yaw = Yaw;
+            dummy.Simulation.Pitch = Pitch;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Dummy/Commands/Actions/CommandDummyLook.cs
@@ -0,0 +1,33 @@
+﻿using Cysharp.Threading.Tasks;
+using Dummy.Actions.Movement.Actions;
+using Dummy.API;
+using Dummy.Users;
+using Microsoft.Extensions.Localization;
+using OpenMod.Core.Commands;
+using System;
+
+namespace Dummy.Commands.Actions
+{
+    [Command("look")]
+    [CommandParent(typeof(CommandDummy))]
+    [CommandSyntax("<yaw> <pitch>")]
+    public class CommandDummyLook : CommandDummyAction
+    {
+        public CommandDummyLook(IServiceProvider serviceProvider, IDummyProvider dummyProvider,
+            IStringLocalizer stringLocalizer) : base(serviceProvider, dummyProvider, stringLocalizer)
+        {
+        }
+
+        protected override async UniTask ExecuteDummyAsync(DummyUser playerDummy)
+        {
+            if (Context.Parameters.Count < 3)
+            {
+                throw new CommandWrongUsageException(Context);
+            }
+            var yaw = await Context.Parameters.GetAsync<float>(1);
+            var pitch = await Context.Parameters.GetAsync<float>(2);
+
+            playerDummy.Actions.Actions.Enqueue(new RotateAction(yaw, pitch));
+        }
+    }
+}
