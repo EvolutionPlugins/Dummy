7fb661b66bbf5ada610c6e80db8f3df4e0629eff
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2022-10-13 20:17:13 +0700

    Fix dummy moves fast

---
+++ Dummy/Patches/Patch_InteractableVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HarmonyLib;
+using SDG.Unturned;
+using UnityEngine;
+
+namespace Dummy.Patches;
+[HarmonyPatch(typeof(InteractableVehicle))]
+internal static class Patch_InteractableVehicle
+{
+    [HarmonyReversePatch]
+    [HarmonyPatch("PackRoadPosition")]
+    public static Vector3 PackRoadPositionOriginal(float roadPosition)
+    {
+        throw new NotImplementedException("It's a stub");
+    }
+}
