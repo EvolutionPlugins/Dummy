a03c36345c3b623cb1bf95dc5844dbc23fdbf125
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2026-07-25 16:02:32 +0700

    Remove battleye id use

--- Dummy/Patches/Patch_Provider.cs
+++ Dummy/Patches/Patch_Provider.cs
@@ -1,15 +1,13 @@
-﻿extern alias JetBrainsAnnotations;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
-using JetBrainsAnnotations::JetBrains.Annotations;
 using SDG.Unturned;
+using Serilog;
 
 namespace Dummy.Patches
 {
-    // ReSharper disable InconsistentNaming
     [HarmonyPatch(typeof(Provider))]
     internal static class Patch_Provider
     {
@@ -20,21 +18,19 @@ namespace Dummy.Patches
             return OnNeedDummy?.Invoke().Dummies.Count ?? 0;
         }
 
+        [HarmonyCleanup]
+        public static Exception? Cleanup(Exception exception)
+        {
+            Log.Error(exception, "Failed to patch\n{Stacktrace}", Environment.StackTrace);
+            return null;
+        }
+
         internal static readonly MethodInfo s_GetClients = AccessTools.DeclaredPropertyGetter(typeof(Provider), "clients");
         internal static readonly MethodInfo s_GetDummiesCount = SymbolExtensions.GetMethodInfo(() => GetDummiesCount());
 
-        [HarmonyPrefix]
-        [HarmonyPatch("battlEyeServerKickPlayer")]
-        [UsedImplicitly]
-        public static bool battlEyeServerKickPlayer(int playerID)
-        {
-            return !(OnNeedDummy?.Invoke().Dummies.Any(x => x.Player.BattlEyeId == playerID) ?? false);
-        }
-
         [HarmonyTranspiler]
         [HarmonyPatch("verifyNextPlayerInQueue")]
-        [UsedImplicitly]
-        public static IEnumerable<CodeInstruction> verifyNextPlayerInQueue(IEnumerable<CodeInstruction> instructions)
+        public static IEnumerable<CodeInstruction> VerifyNextPlayerInQueue(IEnumerable<CodeInstruction> instructions)
         {
             var codes = new List<CodeInstruction>(instructions);
             for (var i = 0; i < codes.Count; i++)
--- Dummy/Players/DummyPlayer.cs
+++ Dummy/Players/DummyPlayer.cs
@@ -1,35 +1,32 @@
-﻿using Dummy.Users;
-using HarmonyLib;
+﻿using System;
+using Dummy.Users;
 using OpenMod.Unturned.Players;
 using SDG.Unturned;
-using System;
-using System.Reflection;
 
 namespace Dummy.Players
 {
     public class DummyPlayer : UnturnedPlayer, IEquatable<DummyUser>
     {
-        private static readonly FieldInfo s_BattlEyeId = AccessTools.Field(typeof(SteamPlayer), "battlEyeId");
-
-        public int BattlEyeId { get; }
-
         public DummyPlayer(SteamPlayer steamPlayer) : base(steamPlayer.player)
         {
-            BattlEyeId = (int)s_BattlEyeId.GetValue(steamPlayer);
         }
 
         public bool Equals(DummyUser other)
         {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
 
             return other.SteamID.Equals(SteamId);
         }
 
         public override bool Equals(object? obj)
         {
-            if (obj is DummyPlayer other) return Equals(other);
-            else return false;
+            if (obj is DummyPlayer other)
+                return Equals(other);
+            else
+                return false;
         }
 
         public override int GetHashCode()
